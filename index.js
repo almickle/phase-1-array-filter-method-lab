8be9a1be9c579d7662cdfb26d0fc09f2855dadf9
Michael Dear <michaelcdear@gmail.com>
2022-06-29 12:23:40 -0600

    commit tree b7e3deb18436e48c23a23f62ac95cc28924626b0

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Code your solution here
+
+function findMatching(array, string) {
+    return array.filter(element => element.toUpperCase() === string.toUpperCase())
+}
+
+function fuzzyMatch(array, string) {
+    return array.filter(element => element.startsWith(string))
+}
+
+function matchName(array, string) {
+    return array.filter(element => element.name === string)
+}
